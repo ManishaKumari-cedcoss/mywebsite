0b525361065bd81c441139d50e133274886c0960
unknown <manishakumari@cedcoss.com>
2020-06-16 21:00:58 +0530

    change option

--- wp-content/plugins/myplugin/plugin-register.php
+++ wp-content/plugins/myplugin/plugin-register.php
@@ -15,47 +15,15 @@
  * Domain Path:       /languages
  */
 
- /**
-  * Register the "book" custom post type
-  */
-function pluginprefix_setup_post_type() {
-	register_post_type( 'book', ['public' => true ] ); 
-} 
-add_action( 'init', 'pluginprefix_setup_post_type' );
-	/**
-	 * Activate the plugin.
-	 */
-function pluginprefix_activate() { 
-		// Trigger our function that registers the custom post type plugin.
-		pluginprefix_setup_post_type(); 
-		// Clear the permalinks after the post type has been registered.
-		flush_rewrite_rules(); 
-}
-	register_activation_hook( __FILE__, 'pluginprefix_activate' );
-
+ 
     /**
 	 * time of the plugin.
 	 */
-
-	function add_my_menu(){
-		add_menu_page(
-		"Budberg","BudBerg","manage_options","hello-world","add_function","dashicons-visibility",);
+	register_activation_hook( __FILE__, 'budberg_active' );
+	function budberg_active() {
+		add_option( 'budberg_active', 'activated' );
 	}
-	add_action("admin_menu","add_my_menu");
-	
-	function add_function(){
-		echo "Activation time ";
-		$date=date("h:i:sa");
-		echo $date;
+	function budberg_active() {
+		delete_option( 'budberg_active' );
 	}
-
-	/**
-	 * Deactivation hook.
-	 */
-function pluginprefix_deactivate() {
-		// Unregister the post type, so the rules are no longer in memory.
-		unregister_post_type( 'book' );
-		// Clear the permalinks to remove our post type's rules from the database.
-		flush_rewrite_rules();
-}
-register_deactivation_hook( __FILE__, 'pluginprefix_deactivate' );
+	register_deactivation_hook( __FILE__, 'budberg_active' );
